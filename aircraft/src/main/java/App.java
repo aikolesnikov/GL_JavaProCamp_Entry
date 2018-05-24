8d68466851c5912784c5f8aa2de95aeaa3f6b9f7
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2018-05-24 14:52:26 +0300

    db 3

--- aircraft/src/main/java/App.java
+++ aircraft/src/main/java/App.java
@@ -4,29 +4,32 @@ import java.io.IOException;
 import java.sql.*;
 import java.util.Properties;
 
-public class TryDB {
 
+public class App {
+
+    private final static Properties appProps = new Properties();
     private final static String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
     private final static String appConfigPath = rootPath + ".." + File.separator + ".." + File.separator + "app.properties";
-    private final static Properties appProps = new Properties();
 
 
-    public static void main(String[] args) throws SQLException {
-
+    static {
         try {
             appProps.load(new FileInputStream(appConfigPath));
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(1);
         }
+    }
 
-        String db = appProps.getProperty("DB_URL");
-        String db_login = appProps.getProperty("DB_Login");
-        String db_password = appProps.getProperty("DB_Password");
+    final static String db = appProps.getProperty("DB_URL");
+    final static String db_login = appProps.getProperty("DB_Login");
+    final static String db_password = appProps.getProperty("DB_Password");
 
-        try (Connection connection = DriverManager.getConnection(db, db_login, db_password); Statement stmt = connection.createStatement()) {
-            ResultSet rs = stmt.executeQuery("select * from tbl_aEngines");
+    public static void main(String[] args) throws SQLException {
 
+        ResultSet rs = DB_Worker.getData("select * from tbl_aEngines");
+
+        try {
             while (rs.next()) {
                 System.out.println(
                         rs.getInt("id") + ",  " +
@@ -45,8 +48,9 @@ public class TryDB {
                 System.out.println();
             }
 
-        } catch (SQLException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
 }
